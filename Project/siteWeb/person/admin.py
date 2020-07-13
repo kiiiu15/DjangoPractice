90fe017f4cfdfd4f62e9da0a6058fa4a690a1d4f
kiiiu15 <tinbulzomi@hotmail.com>
2020-07-13 20:05:18 -0300

    Set database and admin users

--- Project/siteWeb/person/admin.py
+++ Project/siteWeb/person/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Person, Pet
+
+admin.site.register(Person)
+admin.site.register(Pet)
--- Project/siteWeb/person/models.py
+++ Project/siteWeb/person/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=50, default='')
+    lastName = models.CharField(max_length=50, default='')
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{"name": "%s", "lastName" : "%s", "age" : "%d"}' % (self.name, self.lastName, self.age)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    weight = models.IntegerField(default=0)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{"name": "%s", "weight" : "%d", "owner" : "%s"}' % (self.name, self.weight, self.owner.__str__())
