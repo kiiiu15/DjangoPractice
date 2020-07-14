9eb6b51e6ce83338271d499dfa6a9d4e6596fa23
kiiiu15 <tinbulzomi@hotmail.com>
2020-07-14 17:38:20 -0300

    first template

--- Project/siteWeb/person/urls.py
+++ Project/siteWeb/person/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name='person'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:idPerson>', views.getById, name='getById'),
 ]
--- Project/siteWeb/person/views.py
+++ Project/siteWeb/person/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Este es el index de person")
+    var = "Texto de prueba para ver que se puede imprimir desde pyhton"
+    return render(request, 'person/index.html', {
+        'var': var
+    })
+
+
+def getById(request, idPerson):
+    return HttpResponse("Accediste a person: %s" % idPerson)
